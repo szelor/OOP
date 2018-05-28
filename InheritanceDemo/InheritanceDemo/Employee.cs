10ca9496d5d91c13cc779310baf5c6307df9a566
Szelor <szelor@users.noreply.github.com>
2018-05-28 07:59:01 +0200

    Add files via upload

---
+++ InheritanceDemo/InheritanceDemo/Employee.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritanceDemo
+{
+   public class Employee
+   {
+      public string Name { get; set; }
+      protected double vacationDays;
+      public virtual void TakeVacation() { }
+
+      public Employee(string name)
+      {
+         Name = name;
+      }
+
+      public override string ToString()
+      {
+         return $"[Employee: Name = {Name}]";
+      }
+
+      
+   }
+}
