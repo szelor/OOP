a6b7620c0ff30b18d746f427c20dd940718be61c
Szelor <szelor@users.noreply.github.com>
2018-05-28 08:03:27 +0200

    Add files via upload

---
+++ References/ScrapeLibrary/Scrape.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScrapeLibrary
+{
+    public class Scrape
+    {
+        public string ScrapeWebpage(string url)
+        {
+            return GetWebpage(url);
+        }
+
+        public string ScrapeWebpage(string url, string filepath)
+        {
+            string reply = GetWebpage(url);
+
+            File.WriteAllText(filepath, reply);
+            return reply;
+        }
+
+        private string GetWebpage(string url)
+        {
+            WebClient client = new WebClient();
+            string content = client.DownloadString(url);
+            content += "THAT'S ALL FOLKS!!!";
+            return content;
+        }
+
+    }
+}
